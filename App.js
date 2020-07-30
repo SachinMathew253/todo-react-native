b836c8d0b1ae2a88e51b7c1d46ed2fb5e24b14a4
SachinMathew253 <sachinsaji1999@gmail.com>
2020-07-31 04:33:30 +0530

    Changes to be committed: 	modified:   App.js 	modified:   app.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/LoginScreenComponent.js 	modified:   src/SingleNoteSummaryComponent.js

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import NotesScreenComponent from './src/NotesScreenComponent';
+import firebase from 'firebase';
+import LoginScreenComponent from './src/LoginScreenComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <NotesScreenComponent/>
-    </View>
-  );
-}
+  const [userLoggedIn, setUserLoggedIn] = useState(false)
+  
+  if(firebase.apps.length === 0){
+    var firebaseConfig = {
+      apiKey: "AIzaSyCB2TP9dGmVRg8hAv-_Us73R5iDR-4FvKI",
+      authDomain: "rn-masterclass-todo.firebaseapp.com",
+      databaseURL: "https://rn-masterclass-todo.firebaseio.com",
+      projectId: "rn-masterclass-todo",
+      storageBucket: "rn-masterclass-todo.appspot.com",
+      messagingSenderId: "635976787234",
+      appId: "1:635976787234:web:43d9006c80a66c812ef17b"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  firebase.auth().onAuthStateChanged((user) => {
+    if(user === null) {
+      setUserLoggedIn(false)
+    } else {
+      setUserLoggedIn(true)
+    }
+  })
+
+  if(userLoggedIn) {
+    return (
+      <View>
+        <NotesScreenComponent/>
+        {/* <LoginScreenComponent/> */}
+      </View>
+    );
+  } else {
+    return (
+      <View>
+        {/* <NotesScreenComponent/> */}
+        <LoginScreenComponent/>
+      </View>
+    );
+  }
+}
